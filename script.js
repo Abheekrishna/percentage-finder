c39b68384c1f1736333e76963a6c25f2e02eb317
Abheekrishna <abheekrishna17@gmail.com>
2023-07-06 19:12:42 +0530

    FIxed styling problem

--- script.js
+++ script.js
@@ -7,16 +7,25 @@ const inputDiv = document.getElementById('inputs');
 const createInputs = (count) => {
     if(count > 0) {
         for(let i = 0; i < count; i++) {
+            const inputsDiv = document.createElement('div');
+
             const input = document.createElement('input');
             const totalScore = document.createElement('input');
+
+
             input.placeholder = 'Score';
             input.type = 'number';
             totalScore.placeholder = 'Total';
             totalScore.type = 'number';
+
+
             input.classList.add('input');
             totalScore.classList.add('inputTotal');
-            inputDiv.appendChild(input);
-            inputDiv.appendChild(totalScore);
+
+            inputsDiv.appendChild(input);
+            inputsDiv.appendChild(totalScore);
+
+            inputDiv.appendChild(inputsDiv);
         }
     } else {
         return;
