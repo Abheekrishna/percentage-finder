b8dea67da0fa4c80e4d8ea6d8bf93ca7f6f9f344
Abheekrishna <abheekrishna17@gmail.com>
2023-07-06 18:28:32 +0530

    Added input creation function with value from user

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const subjectCount = document.getElementById('subjectCount');
 const submitBtn = document.getElementById('submit');
+const inputDiv = document.getElementById('inputs');
 
 
-const getSubjectCount =  () => {
-    const subjects = subjectCount.value;
-    return subjects;
+
+const createInputs = (count) => {
+    if(count > 0) {
+        for(let i = 0; i < count.length; i++) {
+            const input = document.createElement('input');
+            input.style.backgroundColor = 'red';
+            inputDiv.appendChild(input);
+        }
+    } else {
+        return;
+    }
 }
 
+
+submitBtn.addEventListener('click', () => {
+    console.log()
+    createInputs(subjectCount.value)
+})
