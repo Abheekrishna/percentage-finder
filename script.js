a0214bcafb915eac9e4ef9805be1477367e94824
Abheekrishna <abheekrishna17@gmail.com>
2023-07-06 18:16:54 +0530

    Created get subject function to get value

---
+++ script.js
@@ -0,0 +1,9 @@
+const subjectCount = document.getElementById('subjectCount');
+const submitBtn = document.getElementById('submit');
+
+
+const getSubjectCount =  () => {
+    const subjects = subjectCount.value;
+    return subjects;
+}
+
