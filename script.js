cb5fbe18ba91489a729e363612d44878f58eaa9e
Abheekrishna <abheekrishna17@gmail.com>
2023-07-06 19:14:51 +0530

    Removed defaults

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const inputDiv = document.getElementById('inputs');
 
 const createInputs = (count) => {
     if(count > 0) {
+
+        //Creates inputs according to the user's input
+
         for(let i = 0; i < count; i++) {
             const inputsDiv = document.createElement('div');
 
@@ -34,6 +37,6 @@ const createInputs = (count) => {
 
 
 submitBtn.addEventListener('click', () => {
-    console.log()
-    createInputs(subjectCount.value)
+    createInputs(subjectCount.value);
+    subjectCount.value = ''
 })
